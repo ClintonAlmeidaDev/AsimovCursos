afe003169691d7e0158f915d406593556bfe3c54
Clinton <clinton.almeida10@gmail.com>
2025-01-24 19:24:31 -0300

    Adicionando projeto de integracao com IA. Groq

---
+++ Python com IA/aprendizado.py
@@ -0,0 +1,11 @@
+
+import os
+from langchain_groq import ChatGroq
+
+api_key = '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'
+os.environ['GROQ_API_KEY'] = api_key
+
+chat = ChatGroq(model='llama-3.2-1b-preview')
+
+resposta = chat.invoke('Quanto é 2 + 2')
+print(resposta.content)
